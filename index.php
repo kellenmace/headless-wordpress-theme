e69371aa3194e5d5044aa5f22e6d625ebe8c528b
Kellen Mace <kellen.mace@gmail.com>
2020-10-26 11:22:19 -0400

    Add proper html markup to index page template

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
-<?php
-/**
- * Render a link that points to the wp-admin.
- */
-?>
-
-<a href="<?php echo esc_url( admin_url() ); ?>">
-    <?php echo esc_html( get_bloginfo( 'name' ) ); ?> admin
-</a>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <!-- Render a link that points to the wp-admin. -->
+        <a href="<?php echo esc_url( admin_url() ); ?>">
+            <?php echo esc_html( get_bloginfo( 'name' ) ); ?> admin
+        </a>
+    </body>
+</html>
